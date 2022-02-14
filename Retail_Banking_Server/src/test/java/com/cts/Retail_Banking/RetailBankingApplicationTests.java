052786871d0fb384709a1fcaa17bddeb8f9dd0d8
Huncho Jack <meniman98@gmail.com>
2022-02-14 16:56:34 +0100

    added banking app

---
+++ Retail_Banking_Server/src/test/java/com/cts/Retail_Banking/RetailBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.Retail_Banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

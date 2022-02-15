bbbe1cfc9c6ac9f9cb4c922ced9c3a434a9e7611
Huncho Jack <meniman98@gmail.com>
2022-02-15 14:06:58 +0100

    Made the customer and account pojo

--- Account/Account/src/main/java/com/cts/Account/model/Account.java
+++ Account/Account/src/main/java/com/cts/Account/model/Account.java
@@ -1,4 +1,23 @@
 package com.cts.Account.model;
 
+import lombok.Data;
+
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Data
 public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private short accountId;
+    private String accountNo;
+    private LocalDate dateOfCreation;
+    private String accountType;
+    private double balance;
+    private Customer customerId;
+
 }
--- Account/Account/src/main/java/com/cts/Account/model/Customer.java
+++ Account/Account/src/main/java/com/cts/Account/model/Customer.java
@@ -1,4 +1,21 @@
 package com.cts.Account.model;
 
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Data
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private short customerId;
+    private String name;
+    private LocalDate localDate;
+    private String address;
 }

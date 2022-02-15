c06080b7084cad51bc945fd8d52865ff64f9aa8d
Huncho Jack <meniman98@gmail.com>
2022-02-15 10:46:45 +0100

    test commit Ahmed

--- Retail_Banking_Server/src/main/java/com/cts/Retail_Banking/RetailBankingApplication.java
+++ Retail_Banking_Server/src/main/java/com/cts/Retail_Banking/RetailBankingApplication.java
@@ -8,6 +8,7 @@ public class RetailBankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetailBankingApplication.class, args);
+//		test comment from Ahmed
 	}
 
 }

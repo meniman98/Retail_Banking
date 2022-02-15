a16c62aeab1f4a787cddfe9f17f1a4bc2dec013f
Huncho Jack <meniman98@gmail.com>
2022-02-15 14:12:03 +0100

    made the statement pojo

--- Account/Account/src/main/java/com/cts/Account/model/Statement.java
+++ Account/Account/src/main/java/com/cts/Account/model/Statement.java
@@ -1,4 +1,22 @@
 package com.cts.Account.model;
 
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Data
 public class Statement {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private short statementId;
+    private String refNo;
+    private Account accountId;
+    private LocalDate date;
+    private double withdrawal;
+    private LocalDate valueDate;
+//    TODO: add narration
 }

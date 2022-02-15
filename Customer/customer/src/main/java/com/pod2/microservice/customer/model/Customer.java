2a2755eb091f310e3b68e4b98c9fcaf77ed3a81e
Enjana-mavitrika <rabonarijaona.john@gmail.com>
2022-02-15 17:04:34 +0100

    set up MySQL connection and create Entities

--- Customer/customer/src/main/java/com/pod2/microservice/customer/model/Customer.java
+++ Customer/customer/src/main/java/com/pod2/microservice/customer/model/Customer.java
@@ -1,5 +1,29 @@
 package com.pod2.microservice.customer.model;
 
-public class Customer {
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+
+@Entity
+@Data
+public class Customer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long customerId;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String mobileNumber;
+	private String address;
+	private String dateOfBirth;
+	
+	@OneToMany(fetch=FetchType.EAGER)
+	private List<CustomerCreationStatus> customerCreationStatus;
 }

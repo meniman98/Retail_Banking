be6664af37078773f5f63a4c1362d9f3de049da4
Enjana-mavitrika <rabonarijaona.john@gmail.com>
2022-02-16 18:05:23 +0100

    update unit test and code for customer rest controller

--- Customer/customer/src/main/java/com/pod2/microservice/customer/controller/CustomerRestController.java
+++ Customer/customer/src/main/java/com/pod2/microservice/customer/controller/CustomerRestController.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,12 +20,27 @@ public class CustomerRestController {
 	@Autowired
 	private CustomerService customerService;
 	
-	@PostMapping("/createCustomer")
+	@PostMapping("${url.create.customer}")
 	public @ResponseBody CustomerCreationStatus createCustomer(@RequestBody Customer customer, HttpServletResponse httpResponse) {
 		CustomerCreationStatus status = this.customerService.create(customer);
 		if (null != status) {
 			httpResponse.setStatus(HttpStatus.CREATED.value());
+		} else {
+			httpResponse.setStatus(HttpStatus.BAD_REQUEST.value());
 		}
+		
 		return status;
+	}
+	
+	@GetMapping("${url.details.customer}")
+	public @ResponseBody Customer getCustomerDetails(@RequestBody Long customerId, HttpServletResponse httpResponse) {
+		Customer customer = this.customerService.getDetailsById(customerId);
+		if (null != customer) {
+			httpResponse.setStatus(HttpStatus.OK.value());
+		} else {
+			httpResponse.setStatus(HttpStatus.NOT_FOUND.value());
+		}
+		
+		return customer;
 	}
 }
--- Customer/customer/src/test/java/com/pod2/microservice/customer/controller/CustomerRestControllerTest.java
+++ Customer/customer/src/test/java/com/pod2/microservice/customer/controller/CustomerRestControllerTest.java
@@ -1,5 +1,6 @@
 package com.pod2.microservice.customer.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -10,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -32,33 +34,89 @@ public class CustomerRestControllerTest {
 	@Autowired
 	private Gson gson;
 
+	@Value("${url.create.customer}")
+	private String URL_CREATE_CUSTOMER;
+	@Value("${url.details.customer}")
+	private String URL_DETAILS_CUSTOMER;
 	
 	private Customer validCustomer = new Customer(1L, "Test", "Test", "test@test.com", "0123456789", "1, Test France",
 			"01/01/1998");
 	private Customer invalidCustomer = new Customer(0L, "", "", "", "", "1, Test France", "01/01/1998");
 	private CustomerCreationStatus customerCreationStatus = new CustomerCreationStatus(1L, CustomerServiceImpl.MSG_CUSTOMER_CREATION_SUCCESS);
 	
+	private final Long SAVED_CUSTOMER_ID = 1L;
+	private final Long UNSAVED_CUSTOMER_ID = 5L;
+	
 	@BeforeEach
 	public void setUp() throws Exception {
 		MockitoAnnotations.initMocks(this);
 		// Mock CustomerService
 		Mockito.when(this.customerService.create(this.validCustomer)).thenReturn(this.customerCreationStatus);
+		Mockito.when(this.customerService.create(this.invalidCustomer)).thenReturn(null);		
+		Mockito.when(this.customerService.getDetailsById(this.SAVED_CUSTOMER_ID)).thenReturn(this.validCustomer);		
+		Mockito.when(this.customerService.getDetailsById(this.UNSAVED_CUSTOMER_ID)).thenReturn(null);		
 	}
 	
-//	@Test
-//	public void createCustomerWithValidCustomerShouldReturnOkStatus() throws Exception {
-//		this.mockMvc.perform(post("/createCustomer").contentType(MediaType.APPLICATION_JSON).content().andDo(print()).andExpect(status().isOk());
-//	}
-	
 	@Test
-	public void createCustomerWithValidCustomerShouldReturnObject() throws Exception {
+	public void createCustomerWithValidCustomerShouldReturnIsCreatedStatus() throws Exception {
 		String customerJson = this.gson.toJson(this.validCustomer);
-		this.mockMvc.perform(post("/createCustomer")
+		this.mockMvc.perform(post(this.URL_CREATE_CUSTOMER)
 				.contentType(MediaType.APPLICATION_JSON)
 				.content(customerJson).accept(MediaType.APPLICATION_JSON))
-		.andExpect(status().isCreated())
+		.andExpect(status().isCreated());
+	}
+	
+	@Test
+	public void createCustomerWithInvalidCustomerShouldReturnBadRequestStatus() throws Exception {
+		String invalidCustomerJson = this.gson.toJson(this.invalidCustomer);
+		this.mockMvc.perform(post(this.URL_CREATE_CUSTOMER)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(invalidCustomerJson).accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isBadRequest());
+	}
+	
+	@Test
+	public void createCustomerWithValidCustomerShouldReturnCreationStatusObject() throws Exception {
+		String customerJson = this.gson.toJson(this.validCustomer);
+		this.mockMvc.perform(post(this.URL_CREATE_CUSTOMER)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(customerJson).accept(MediaType.APPLICATION_JSON))
 		.andExpect(content().contentType(MediaType.APPLICATION_JSON))
 		.andExpect(jsonPath("$.id").value(this.customerCreationStatus.getId()))
 		.andExpect(jsonPath("$.message").value(this.customerCreationStatus.getMessage()));
+	}
+	
+	@Test
+	public void getCustomerDetailsWithExistingCustomerShouldReturnStatusOk() throws Exception {
+		String customerIdJson = this.gson.toJson(this.SAVED_CUSTOMER_ID);
+		this.mockMvc.perform(get(this.URL_DETAILS_CUSTOMER)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(customerIdJson).accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isOk());
+	}
+	
+	@Test
+	public void getCustomerDetailsWithNoneExistingCustomerShouldReturnStatusNotFound() throws Exception {
+		String customerIdJson = this.gson.toJson(this.UNSAVED_CUSTOMER_ID);
+		this.mockMvc.perform(get(this.URL_DETAILS_CUSTOMER)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(customerIdJson).accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void getCustomerDetailsWithExistingCustomerShouldReturnCustomerObject() throws Exception {
+		String customerIdJson = this.gson.toJson(this.SAVED_CUSTOMER_ID);
+		this.mockMvc.perform(get(this.URL_DETAILS_CUSTOMER)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(customerIdJson).accept(MediaType.APPLICATION_JSON))
+		.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$.customerId").value(this.validCustomer.getCustomerId()))
+		.andExpect(jsonPath("$.dateOfBirth").value(this.validCustomer.getDateOfBirth()))
+		.andExpect(jsonPath("$.address").value(this.validCustomer.getAddress()))
+		.andExpect(jsonPath("$.mobileNumber").value(this.validCustomer.getMobileNumber()))
+		.andExpect(jsonPath("$.email").value(this.validCustomer.getEmail()))
+		.andExpect(jsonPath("$.lastName").value(this.validCustomer.getLastName()))
+		.andExpect(jsonPath("$.firstName").value(this.validCustomer.getFirstName()));
 	}
 }

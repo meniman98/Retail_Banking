96371bba7eadb2538087bff2d6ea9e14d3b15848
Enjana-mavitrika <rabonarijaona.john@gmail.com>
2022-02-18 14:11:16 +0100

    Add logging using slf4j

--- Rule/rules/src/main/java/com/pod2/microservice/rules/config/SleuthConfig.java
+++ Rule/rules/src/main/java/com/pod2/microservice/rules/config/SleuthConfig.java
@@ -1,12 +1,12 @@
-//package com.pod2.microservice.rules.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//
-//import brave.sampler.Sampler;
-//
-//@Configuration
-//public class SleuthConfig {
-//	public Sampler defaultSampler() {
-//		return Sampler.ALWAYS_SAMPLE;
-//	}
-//}
+package com.pod2.microservice.rules.config;
+
+import org.springframework.context.annotation.Configuration;
+
+import brave.sampler.Sampler;
+
+@Configuration
+public class SleuthConfig {
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+}
--- Rule/rules/src/main/java/com/pod2/microservice/rules/service/BusinessRulesEngineImpl.java
+++ Rule/rules/src/main/java/com/pod2/microservice/rules/service/BusinessRulesEngineImpl.java
@@ -3,6 +3,8 @@ package com.pod2.microservice.rules.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,8 @@ import com.pod2.microservice.rules.model.RuleStatus;
 
 @Service
 public class BusinessRulesEngineImpl implements BusinessRulesEngine {
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@Value("${rules.status.accepted:allowed}")
 	public String ACCEPTED_STATUS = "allowed";
 	@Value("${rules.status.rejected:denied}")
@@ -28,16 +32,20 @@ public class BusinessRulesEngineImpl implements BusinessRulesEngine {
 	@Override
 	public RuleStatus evaluate(double balance, String accountType) {
 		if (!this.minBalanceAccountTypeMap.keySet().contains(accountType)) {
+			this.log.info("Rejected evaluation for balance = " + balance + " and accountType = " + accountType);
 			return new RuleStatus(this.REJECTED_STATUS);
 		}
 		if (balance >= this.minBalanceAccountTypeMap.get(accountType)) {
+			this.log.info("Accepted evaluation for balance = " + balance + " and accountType = " + accountType);
 			return new RuleStatus(this.ACCEPTED_STATUS);
 		}
+		this.log.info("Rejected evaluation for balance = " + balance + " and accountType = " + accountType);
 		return new RuleStatus(this.REJECTED_STATUS);
 	}
 
 	@Override
 	public float getServiceCharges() {
+		this.log.info("Send service charges = " + this.chargesFix);
 		return this.chargesFix;
 	}
 

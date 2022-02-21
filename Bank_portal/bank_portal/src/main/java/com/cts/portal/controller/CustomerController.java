dee53f14e9d99bf8862dfe5526f98b3776a46b45
Enjana-mavitrika <rabonarijaona.john@gmail.com>
2022-02-21 20:57:02 +0100

    Add server side validation to create customer form inputs

--- Bank_portal/bank_portal/src/main/java/com/cts/portal/controller/CustomerController.java
+++ Bank_portal/bank_portal/src/main/java/com/cts/portal/controller/CustomerController.java
@@ -1,13 +1,41 @@
 package com.cts.portal.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.cts.portal.dto.Customer;
 
 @Controller
 public class CustomerController {
+	
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, false));
+	}
+	
 	@GetMapping("/createCustomer")
-	public String getCreateCustomerAction() {
+	public String getCreateCustomerAction(ModelMap model, Customer customer) {
 		return "create-customer";
+	}
+	
+	@PostMapping("/createCustomer")
+	public String postCreateCustomerAction(ModelMap model, @Valid Customer customer, BindingResult result) {
+		if (result.hasErrors()) {
+			return "create-customer";
+		}
+		return "redirect:/customers";
 	}
 	
 	@GetMapping("/getCustomer")

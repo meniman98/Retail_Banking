10accb556f06d5c93cc6b26e5cd4d9d346890d06
Enjana-mavitrika <rabonarijaona.john@gmail.com>
2022-02-22 23:12:29 +0100

    Fix interraction between Account and Customer microservices

--- Account/Account/src/main/java/com/cts/Account/service/AccountService.java
+++ Account/Account/src/main/java/com/cts/Account/service/AccountService.java
@@ -5,11 +5,11 @@ import com.cts.Account.model.AccountCreationStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
 import java.util.Set;
 
 public interface AccountService {
-    AccountCreationStatus createAccount(@PathVariable Long customerId,
-                                        @RequestBody Account account);
-    Set<Account> getCustomerAccounts(@PathVariable Long customerId);
+    AccountCreationStatus createAccount(@PathVariable Long customerId);
+    List<Account> getCustomerAccounts(@PathVariable Long customerId);
     Account getAccount(@PathVariable Long accountId);
 }

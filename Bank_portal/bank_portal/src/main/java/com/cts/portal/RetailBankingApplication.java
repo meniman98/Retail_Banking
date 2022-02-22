69eae93be7303b1b78bd690236270db721709130
Enjana-mavitrika <rabonarijaona.john@gmail.com>
2022-02-22 12:01:52 +0100

    Add create customer form validation and controller

--- Bank_portal/bank_portal/src/main/java/com/cts/portal/RetailBankingApplication.java
+++ Bank_portal/bank_portal/src/main/java/com/cts/portal/RetailBankingApplication.java
@@ -3,15 +3,15 @@ package com.cts.portal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.cts.portal")
 @EnableEurekaServer
 public class RetailBankingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetailBankingApplication.class, args);
-//		test comment from Ahmed
-//		test comment for a branch
 	}
 
 }

2325b58d51850c1e51713870e8fe2f22368586d2
Thomas Jungblut <thomas.jungblut@gmail.com>
2022-03-01 15:42:39 +0100

    advanced mocking

--- Account/Account/src/test/java/com/cts/Account/AccountServiceTest.java
+++ Account/Account/src/test/java/com/cts/Account/AccountServiceTest.java
@@ -4,91 +4,88 @@ import com.cts.Account.model.Account;
 import com.cts.Account.model.AccountCreationStatus;
 import com.cts.Account.repo.AccountRepo;
 import com.cts.Account.service.AccountServiceImpl;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class AccountServiceTest {
-
-    @Mock
-    AccountServiceImpl accountService;
 
     @Mock
     AccountRepo accountRepo;
 
-
-    Account testAccount = new Account(1, 1);
-    Account testAccount2 = new Account(2, 1);
-    AccountCreationStatus testAccountStatus = new AccountCreationStatus(new Account(), null);
-    List<Account> customerAccounts = Arrays.asList(testAccount, testAccount2);
-    List<Account> emptyList = new ArrayList<>();
-
-
-    /*1. create an account
-    * 2. get back that same account with the ID
-    * 3. Make sure that is the same account*/
-
-    @BeforeEach
-    void setUp() {
-        Mockito.when(accountRepo.save(testAccount)).thenReturn(testAccount);
-        Mockito.when(accountRepo.getById(1L)).thenReturn(testAccount);
-
-        Mockito.when(accountService.createAccount(testAccount.getCustomerId())).
-                thenReturn(testAccountStatus);
-
-        Mockito.when(accountService.getAccount(1L)).
-                thenReturn(testAccount);
-
-        Mockito.when(accountService.getCustomerAccounts(1L)).
-                thenReturn(customerAccounts);
-    }
+    @InjectMocks
+    AccountServiceImpl accountService;
 
     @Test
-    void createAccount() {
-        accountService.createAccount(testAccount.getCustomerId());
-//        FIXME: look into why this doesn't get called
-//        Mockito.verify(accountRepo).save(testAccount);
-        assertEquals(accountRepo.getById(testAccount.getAccountId()), testAccount);
+    void createAccountSuccess() {
+        AccountCreationStatus status = accountService.createAccount(42L);
+        // check the "contract" returns what we expect
+        assertNotNull(status);
+        assertEquals("Account successfully created", status.getMessage());
+
+        // check that mock was invoked twice
+        Mockito.verify(accountRepo, Mockito.times(2)).save(Mockito.any());
+
+        // check that a savings and a current account were saved
+        Mockito.verify(accountRepo, Mockito.atLeastOnce()).save(Mockito.argThat(account -> account.getAccountType().equals("Savings")));
+        Mockito.verify(accountRepo, Mockito.atLeastOnce()).save(Mockito.argThat(account -> account.getAccountType().equals("Current")));
     }
 
     @Test
     void failureCreateAccount() {
-//        Mismatching IDs
-        accountService.createAccount(testAccount2.getCustomerId());
-        assertNotEquals(accountRepo.getById(testAccount.getAccountId()), testAccount2);
+        Mockito.when(accountRepo.save(Mockito.any())).thenThrow(new DuplicateKeyException("crash"));
+        // FIXME original ahmed code
+        // Assertions.assertThrows(IllegalArgumentException.class, () -> accountService.createAccount(1L));
+        AccountCreationStatus status = accountService.createAccount(1L);
+        assertNotNull(status);
+        assertEquals("duplicated account id", status.getMessage());
     }
 
     @Test
-    void getCustomerAccounts() {
-        List<Account> returnedAccounts = accountService.getCustomerAccounts(1L);
-        assertEquals(returnedAccounts, customerAccounts);
+    void getCustomerAccount() {
+        Account expectedAccount = new Account(1L, 5L);
+        Mockito.when(accountRepo.getById(1L)).thenReturn(expectedAccount);
+        Mockito.when(accountRepo.existsById(1L)).thenReturn(true);
+
+        Account acc = accountService.getAccount(1L);
+        assertSame(expectedAccount, acc);
+        Mockito.verify(accountRepo, Mockito.atMostOnce()).getById(1L);
     }
 
     @Test
-    void emptyGetCustomerAccounts() {
-        List<Account> returnedAccounts = accountService.getCustomerAccounts(100L);
-        assertEquals(emptyList, returnedAccounts);
+    void getCustomerAccountFails() {
+        Mockito.when(accountRepo.existsById(1L)).thenReturn(false);
+        Assertions.assertThrows(ResponseStatusException.class, () -> accountService.getAccount(1L));
     }
 
     @Test
-    void getSingleCustomerAccount() {
-        Account retrievedAccount = accountService.getAccount(1L);
-        assertEquals(testAccount, retrievedAccount);
+    void getCustomerAccountList() {
+        List<Account> retrievedAccounts = Arrays.asList(new Account(), new Account());
+        Mockito.when(accountRepo.findByCustomerId(1L)).thenReturn(retrievedAccounts);
+        List<Account> list = accountService.getCustomerAccounts(1L);
+        assertSame(retrievedAccounts, list);
     }
 
     @Test
-    void emptyGetSingleCustomerAccount() {
-        Account retrievedAccount = accountService.getAccount(100L);
-        assertNull(retrievedAccount);
+    void getCustomerAccountListEmpty() {
+        List<Account> emptyAccounts = Collections.emptyList();
+        Mockito.when(accountRepo.findByCustomerId(1L)).thenReturn(emptyAccounts);
+        Assertions.assertThrows(ResponseStatusException.class, () -> accountService.getCustomerAccounts(1L));
     }
 
 }
